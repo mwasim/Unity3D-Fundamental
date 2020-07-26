8967a5a07474a61a298b3de77f4c2b5f874716c0
Wasim <mwasimse@gmail.com>
2020-07-26 12:46:16 +0500

    Driving simulation fundamentals

---
+++ DrivingBasics/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset; //distance between camera and player's position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //On the start, we've set the camera offset correctly to the player's position, so let's save that offset
+        /*
+            For example, at the start we know
+            Camera's position = Vector3(0, 5, -10)
+            Player's position = Vector3(0, 0, 0)
+         */
+        offset = transform.position - player.transform.position;
+
+        print($"Offset: ${offset}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //If Camera is little shaking use LateUpdate (it executes after Update is finished)
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ DrivingBasics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float topSpeed = 10;
+    public float turnSpeed = 5;
+
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle forward
+        //transform.Translate(0, 0, 0.1f);
+        //above line can be written as, Vector3.forward = (0, 0, 1), Z = 1
+        transform.Translate(Vector3.forward * Time.deltaTime * topSpeed * forwardInput);
+
+        //This code moves the vehicle left or right
+        //Vector3.up = (1, 0, 0), X = 1
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+        //We need to rotate the vehicle left or right
+        //Vector3.up = (0, 1, 0), Y = 1
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
