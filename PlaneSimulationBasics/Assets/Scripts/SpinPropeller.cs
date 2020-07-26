aeca132e7486dcda6454c73087b58b94bc29b8fd
Wasim <mwasimse@gmail.com>
2020-07-26 16:03:25 +0500

    Plane simulation basics

---
+++ PlaneSimulationBasics/Assets/Scripts/SpinPropeller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+    public float propellerRotationSpeed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotates the propellor on the z axis
+        transform.Rotate(Vector3.forward * propellerRotationSpeed * Time.deltaTime);
+    }
+}
