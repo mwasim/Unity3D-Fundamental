d8c5c630409d723fce8f1c0dfecf9e6f55c95dc1
Wasim <mwasimse@gmail.com>
2020-07-28 20:57:11 +0500

    1. Points in 2D, Cartisian Coordinates, and checking coordinates of a game object within specific aspect ratio and with specific Camera Size. 2). Moving player within boundaries (by clamping only X position)

---
+++ MovingPlayerWithinBoundaries/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 10.0f;
+    public float clampX = 10.0f;
+
+    private float horizontalInput;
+
+    void FixedUpdate()
+    {
+        //STEP-1: MOVE
+        //get horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //move left or right
+        transform.Translate(Vector3.right * horizontalInput * maxSpeed * Time.deltaTime);
+
+
+        //STEP-2: CLAMP to ensure the player moves within the boundary (-X to X)
+        //Ensure player cannot move past to the clampX position
+        if (transform.position.x > clampX)
+            transform.position = new Vector3(clampX, transform.position.y, transform.position.z);
+
+        if (transform.position.x < -clampX)
+            transform.position = new Vector3(-clampX, transform.position.y, transform.position.z);
+    }
+}
