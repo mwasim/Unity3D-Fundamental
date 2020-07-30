089d4694d037becad22c353b3829a8e566d28e27
Wasim <mwasimse@gmail.com>
2020-07-30 05:46:26 +0500

    Using Vectors to move the characters

---
+++ Vectors/Assets/Scripts/Move.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    /*
+        1. Vector has a). ORIGIN b). DESTINATION c) DIRECTION d) Magnitude / Length
+
+        2. Vector length can be calculated as below
+        ||V|| = Sqrt(Squre(X) + SQUARE(Y)) //||V|| = Vector Length
+
+        3. For example, Vector(5,0,4) can have origin (0,0,0) or any other e.g. as set in the scene (-2.58, 0, -2.65)
+           And it's direction is from the Origin towards the Destination. In this example, Vector moves from the origin Vector(-2.58, 0, -2.65)
+           to the Vector(5,0,4) [In the direction of the destination Vector]        
+     */
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.1f; //multiply speed with normalized goal to slow down
+
+    void Start()
+    {
+        //this.transform.Translate(goal);
+
+        //goal = goal * 0.01f; //10 %
+    }
+
+    /*
+        Any movements you'd like to add to the characters or follow the cameras,
+        You should do it when all the Physics has been calculated in the scene,
+        Currently, we don't have Physics so it's not an issue
+
+        But you should get into the habit of putting the movement code into the LateUpdate
+     */
+    private void LateUpdate() 
+    {
+        //this.transform.Translate(goal);
+
+        //normalize makes the vector standard size 1 in the same direction
+        //normalized = Returns the Vector with magnitude = 1
+        //this.transform.Translate(goal.normalized * speed);
+
+        //Update calls are normally inconsistent so multiplying with Time.detaTime smooths it.
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ Vectors/Assets/Scripts/MoveToGoal.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour
+{
+
+    public Transform goal;
+    public float speed = 2.0f;
+    public float accuracy = 0.01f;
+
+    private void Start()
+    {
+        //Character first turns and looks at where it's about to go using LookAt
+        //Give it the goal position, you want it to look at
+        //It'll snap around and look in that direction
+        //transform.LookAt(goal.position);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        //Putting LookAt on LateUpdate makes the character Look at the goal every time goal position's change
+        //For example, within the SceneView (while the game is running), try changing the Goals position by dragging the cube,
+        //and the characters will follow it by first looking at it.
+        transform.LookAt(goal.position);
+
+        //Determine direction by subtracting current position from target position
+        Vector3 direction = goal.position - transform.position;
+
+        //This ray is displayed on within the Scene (not Game view)
+        //Useful for debugging
+        Debug.DrawRay(transform.position, direction, Color.red);
+
+        //Magnitude is the length of th vector
+        if (direction.magnitude > accuracy)
+        {
+            //The below code line will not work properly
+            //transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+            //To fix or make it work you'd like to move the character in the World Space
+            transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
