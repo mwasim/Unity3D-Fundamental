040bfa49624b32a23324822c56fc6132f318eea7
Wasim <mwasimse@gmail.com>
2020-08-05 18:44:16 +0500

    AStar for path finding and using Graphs to move along the way points

---
+++ AI_AStarForPathFinding/Assets/Scripts/WayPointDebug.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class WayPointDebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.parent.gameObject.name != "WayPoint") return;
+
+        RenameWayPoints(null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMesh>().text = transform.parent.gameObject.name;
+    }
+
+
+    private void RenameWayPoints(GameObject overlook)
+    {
+        var wayPoints = GameObject.FindGameObjectsWithTag("WayPoint");
+
+        int index = 1;
+        foreach (var item in wayPoints)
+        {
+            if (item != overlook)
+            {
+                item.name = $"WP {index:000}";
+                index++;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        RenameWayPoints(gameObject);
+    }
+}
