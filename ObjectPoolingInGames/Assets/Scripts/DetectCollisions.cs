8c813b85abd844d4c235fef946a4800cb2732be2
Wasim <mwasimse@gmail.com>
2020-08-15 15:23:38 +0500

    Using object pooling to reuse projectiles instead of creating and destoying them over and over

---
+++ ObjectPoolingInGames/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        // Instead of destroying the projectile when it collides with an animal
+        //Destroy(gameObject); 
+
+        // Just deactivate the food and destroy the animal
+        gameObject.SetActive(false);
+        Destroy(other.gameObject);
+
+    }
+
+}
---
+++ ObjectPoolingInGames/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+	public float speed = 40f;
+
+    void Update()
+    {
+		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
