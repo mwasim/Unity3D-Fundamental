6b044db26b191a4e3bfc595dbb248806722c4c14
Wasim <mwasimse@gmail.com>
2020-08-16 19:44:49 +0500

    AI Path finding using NavMesh

---
+++ AIPathFindingAndCharAnim/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    [SerializeField] Transform goal;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        agent.destination = goal.position;   
+    }
+}
