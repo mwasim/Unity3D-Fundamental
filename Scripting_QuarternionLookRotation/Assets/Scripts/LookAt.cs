553457cfccfa706cee06783ff4102ce3b41c3d27
Wasim <mwasimse@gmail.com>
2020-08-18 13:32:30 +0500

    Scripting Quarternion Look Rotation and ray is also drawn to point to the target while target moves in the scene

---
+++ Scripting_QuarternionLookRotation/Assets/Scripts/LookAt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //LookDirectionToFace = destination - source
+
+        var directionToFace = target.position - transform.position;
+
+        //draw a ray
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+
+
+        //Rotate to look at the target
+        transform.rotation = Quaternion.LookRotation(directionToFace);
+    }
+}
