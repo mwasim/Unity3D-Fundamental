dcbc199080f4896fc2e2270404b12b91abca253c
Wasim <mwasimse@gmail.com>
2020-08-18 13:03:33 +0500

    Scripting destructable crate. Based on this scripting the crate destroys into pieces

---
+++ Scripting_DestructableCrate/Assets/Scripts/Crate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Crate : MonoBehaviour
+{
+    public GameObject fracturedCrate;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var fracturedObject = Instantiate(fracturedCrate, transform.position, Quaternion.identity);
+
+            var rigidBodyList = fracturedObject.GetComponentsInChildren<Rigidbody>();
+
+            if(rigidBodyList.Length > 0)
+            {
+                foreach(var rb in rigidBodyList)
+                {
+                    rb.AddExplosionForce(500, transform.position, 1);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
