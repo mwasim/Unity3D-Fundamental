59fce155ac2cc0075237dd91ebf5148638a5086c
Wasim <mwasimse@gmail.com>
2020-08-18 13:44:58 +0500

    Using Quaternion Slerp to rotate the smoothly facing the target while target moves in the scene

---
+++ Scripting_QauternionSlerp/Assets/Scripts/LookAtSmoothly.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LookAtSmoothly : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //LookDirectionToFace = destination - source
+
+        var directionToFace = target.position - transform.position;
+
+        //draw a ray
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+
+        //Rotate to look at the target
+        var targetRotation = Quaternion.LookRotation(directionToFace);
+
+        //Slerp makes the rotation very smooth (instead of snapping to the target rotation)
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+    }
+}
