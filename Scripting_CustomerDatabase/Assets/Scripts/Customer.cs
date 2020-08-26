1277f36972908c18fc1875c11af070c365714f12
Wasim <mwasimse@gmail.com>
2020-08-26 16:08:01 +0500

    Scripting - Using classes by marking as Serializable to be editable in the Unity's inspector

---
+++ Scripting_CustomerDatabase/Assets/Scripts/Customer.cs
@@ -0,0 +1,27 @@
+﻿/*
+    A class object appears in the Unity's inspector only if it's serializable
+ */
+[System.Serializable]
+public class Customer
+{
+    public string firstName;
+    public string lastName;
+    public int age;
+    public Gender gender;
+    public string occupation;
+
+    public Customer(string fName, string lName, int customerAge, Gender customerGender, string customerOccupation)
+    {
+        firstName = fName;
+        lastName = lName;
+        age = customerAge;
+        gender = customerGender;
+        occupation = customerOccupation;
+    }
+}
+
+public enum Gender
+{
+    Male = 1,
+    Female = 2
+}
---
+++ Scripting_CustomerDatabase/Assets/Scripts/CustomerDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDatabase : MonoBehaviour
+{
+    public List<Customer> customers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        customers = new List<Customer> {
+            new Customer("John", "Corner", 25, Gender.Male, "Designer"),
+            new Customer("James", "Rob", 25, Gender.Male, "Programmer"),
+            new Customer("Jenny", "Cooper", 25, Gender.Female, "QA Tester")
+        };
+
+
+        foreach (var customer in customers)
+        {
+            Debug.Log($"Customer:\tFirst Name: {customer.firstName}, Last Name: {customer.lastName}, Age: {customer.age}, Gender: {customer.gender}, Occupation: {customer.occupation}\n");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
