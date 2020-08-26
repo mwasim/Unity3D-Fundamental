9ccc051916a72d52f167eba9285587d0ace7819b
Wasim <mwasimse@gmail.com>
2020-08-26 12:55:39 +0500

    AI Crowd simulation examples

---
+++ AI_CrowdSimulationExamples/Assets/Scripts/AIController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    public GameObject goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.SetDestination(goal.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
