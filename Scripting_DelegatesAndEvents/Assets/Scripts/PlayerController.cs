39bcabaab6099630c11506d5a75ba034006d3927
Wasim <mwasimse@gmail.com>
2020-09-01 11:55:10 +0500

    Using Delegates/Event and then using Actions as replacement of Delegates/Events. UIManager subscribes to OnDamage event / action and updates the UI as a player receives the damage

---
+++ Scripting_DelegatesAndEvents/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //declare a delegate and an event of type of this delegate
+    public delegate void OnDamageReceived(int currentHealth);
+    public static event OnDamageReceived onDamage;
+
+    //Instead of using the delegate/event syntax above, we can use the Action syntax as below (it's simple and better)
+    public static Action<int> OnDamageReceivedActionSyntax;
+
+    public int Health { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 10;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Damage();
+        }
+    }
+
+    public void Damage()
+    {
+        Health--;
+
+        //if this even was subscribed, invoke it
+        onDamage?.Invoke(Health);
+
+        //The syntax for invoking subscribed Action is also similar
+        OnDamageReceivedActionSyntax?.Invoke(Health);
+
+        //It can be written as below also..
+        //if (onDamage != null)
+        //{
+        //    onDamage(Health);
+    }
+}
---
+++ Scripting_DelegatesAndEvents/Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    //When it's activated or enabled
+    private void OnEnable()
+    {
+        //subscribe to the event
+        PlayerController.onDamage += UpdatePlayerHealth;
+
+        //subcribe to the Action
+        PlayerController.OnDamageReceivedActionSyntax += UpdatePlayerHealth;
+    }
+
+    //When it's deactivated or diabled, ensure to unsubscribe the event
+    private void OnDisable()
+    {
+        //PlayerController.onDamage -= UpdatePlayerHealth;
+
+        PlayerController.OnDamageReceivedActionSyntax -= UpdatePlayerHealth;
+    }    
+
+    private void UpdatePlayerHealth(int currentHealth)
+    {
+        Debug.Log("Current Health: " + currentHealth);
+    }
+}
