a353bd22804b4eee12b0346481328a13fd7f4001
Wasim <mwasimse@gmail.com>
2020-09-03 11:44:00 +0500

    Making a Singleton GameManager, and using the same instance throughout the app

---
+++ Scripting_Singleton/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Singleton (Other classes can access this single instance)
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("GameManager is NULL");
+            }
+
+            return _instance;
+        }
+    }
+
+    //To intialize the Singleton we use the Awake() method
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void DisplayGameTimeElapsed()
+    {
+        Debug.Log("Game time elapsed: 45 seconds");
+    }
+}
---
+++ Scripting_Singleton/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private GameManager _gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //One way is to reference the GameManager script is below
+        //However, as there's only one GameManager, we can make it Singleton
+        //_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+
+        //The better way to access the GameManager is to use it's Singleton instance as below,
+        GameManager.Instance.DisplayGameTimeElapsed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
