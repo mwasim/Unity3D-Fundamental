6e329ebf0c8857cd6fb152a7fb6c9ebaca2a203a
Wasim <mwasimse@gmail.com>
2020-09-20 15:19:34 +0500

    Made the Start and Options menus functional. User can go to options menu by clicking on the options button, and get back by clicking on the Back button

---
+++ MenusInUnity/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main_ViaStartMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+
+        Application.Quit();
+    }
+}
