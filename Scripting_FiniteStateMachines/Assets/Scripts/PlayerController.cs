7252c87060fd3a4dd7b9f3e1b6baad613c517d64
Wasim <mwasimse@gmail.com>
2020-10-09 16:21:49 +0500

    Implemented game mechanics using Non-FSM approach - In Non-Finite-State-Machine based approach implementing even simple behaviors or states becomes fairly complicated

---
+++ Scripting_FiniteStateMachines/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Player Variables
+
+    public float jumpForce;
+    public Transform head;
+    public Transform weapon01;
+    public Transform weapon02;
+
+    public Sprite idleSprite;
+    public Sprite duckingSprite;
+    public Sprite jumpingSprite;
+    public Sprite spinningSprite;
+
+    private SpriteRenderer face;
+    private Rigidbody rbody;
+
+    #endregion
+
+    private bool _isJumping;
+    private bool _isDucking;
+    private bool _isSpinning;
+
+    private float _rotation;
+
+    private void Awake()
+    {
+        face = GetComponentInChildren<SpriteRenderer>();
+        rbody = GetComponent<Rigidbody>();
+        SetExpression(idleSprite);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+            NON-FINITE-STATE MACHINE APPROACH
+         */
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!_isJumping && !_isDucking) //jump if not already jumping or ducking
+            {
+                _isJumping = true;
+
+                SetExpression(jumpingSprite);
+
+                rbody.AddForce(Vector3.up * jumpForce);
+            }
+            else
+            {
+                _isSpinning = true;
+                SetExpression(spinningSprite);
+            }
+        }
+        else if (Input.GetButtonDown("Duck") && !_isJumping) //duck if not already jumping
+        {
+            _isDucking = true;
+
+            head.localPosition = new Vector3(head.localPosition.x, .5f, head.localPosition.z);
+            SetExpression(duckingSprite);
+        }
+        else if (Input.GetButtonUp("Duck") && !_isJumping) //on button release, reset duck
+        {
+            _isDucking = false;
+            head.localPosition = new Vector3(head.localPosition.x, 0.8f, head.localPosition.z);
+            SetExpression(idleSprite);
+        }
+        else if (Input.GetButtonDown("SwapWeapon"))
+        {
+            if (!_isJumping && !_isDucking && !_isSpinning) //jump if not already jumping or ducking
+            {
+                var usingWeapon01 = weapon01.gameObject.activeInHierarchy;
+
+                weapon01.gameObject.SetActive(usingWeapon01 == false);
+                weapon02.gameObject.SetActive(usingWeapon01);
+            }
+        }
+
+        if (_isSpinning)
+        {
+            Spin();
+        }
+    }
+
+    private void Spin()
+    {
+        var amountToRotate = 900 * Time.deltaTime;
+        _rotation += amountToRotate;
+
+        if (_rotation < 360)
+        {
+            transform.Rotate(Vector3.up, _rotation);
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity;
+
+            //reset spinning & rotation
+            _isSpinning = false;
+            _rotation = 0;
+            SetExpression(jumpingSprite);
+        }
+    }
+
+    //As the player collides the ground
+    private void OnCollisionEnter(Collision collision)
+    {
+        _isJumping = false;
+        SetExpression(idleSprite); //reset to idle sprite
+    }
+
+    public void SetExpression(Sprite newExpression)
+    {
+        face.sprite = newExpression;
+    }
+}
---
+++ Scripting_FiniteStateMachines/Assets/Scripts/PlayerController_FSM.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_FSM : MonoBehaviour
+{
+    #region Player Variables
+
+    public float jumpForce;
+    public Transform head;
+    public Transform weapon01;
+    public Transform weapon02;
+
+    public Sprite idleSprite;
+    public Sprite duckingSprite;
+    public Sprite jumpingSprite;
+    public Sprite spinningSprite;
+
+    private SpriteRenderer face;
+    private Rigidbody rbody;
+
+    #endregion
+
+    private void Awake()
+    {
+        face = GetComponentInChildren<SpriteRenderer>();
+        rbody = GetComponent<Rigidbody>();
+        SetExpression(idleSprite);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetExpression(Sprite newExpression)
+    {
+        face.sprite = newExpression;
+    }
+}
