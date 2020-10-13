60fea159045ef2d92bc462c732154ebd48e87974
Wasim <mwasimse@gmail.com>
2020-10-13 14:46:51 +0500

    Created the ISpawns interface and implemented it in the new monobehavior class SpawnItem

---
+++ LootSystem_SwordsAndShovels/Assets/4. Item Loot/Scripts/Interfaces/ISpawns.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISpawns 
+{
+    Rigidbody ItemSpawned { get; set; }
+    Renderer ItemMaterial { get; set; }
+    ItemPickUp ItemType { get; set; }
+
+    void CreateSpawn();
+}
---
+++ LootSystem_SwordsAndShovels/Assets/4. Item Loot/Scripts/Monobehaviors/SpawnItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpawnItem : MonoBehaviour, ISpawns
+{
+    public Rigidbody ItemSpawned { get; set; }
+    public Renderer ItemMaterial { get; set; }
+    public ItemPickUp ItemType { get; set; }
+
+   
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    public void CreateSpawn()
+    {
+        throw new System.NotImplementedException();
+    }
+}
