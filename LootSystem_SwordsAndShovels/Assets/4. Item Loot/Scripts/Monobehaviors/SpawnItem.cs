bb89eeaa7cd11b4a83ad388a90399da82f0b0e9e
Wasim <mwasimse@gmail.com>
2020-10-13 14:57:08 +0500

    Implemented the Spawn logic, and attached the SpwnItem script component to the Chest/Spawner prefab

--- LootSystem_SwordsAndShovels/Assets/4. Item Loot/Scripts/Monobehaviors/SpawnItem.cs
+++ LootSystem_SwordsAndShovels/Assets/4. Item Loot/Scripts/Monobehaviors/SpawnItem.cs
@@ -2,19 +2,46 @@
 
 public class SpawnItem : MonoBehaviour, ISpawns
 {
+    public ItemPickUps_SO[] itemDefinitions;
+
+    private int whichToSpawn = 0; //acts as index
+    private int totalSpawnWeight = 0;
+    private int chosen = 0; //compares itself with `whichToSpawn` to determine which item to spawn
+
     public Rigidbody ItemSpawned { get; set; }
     public Renderer ItemMaterial { get; set; }
     public ItemPickUp ItemType { get; set; }
 
-   
+
     // Start is called before the first frame update
     private void Start()
     {
-        
+        foreach (var ip in itemDefinitions)
+        {
+            totalSpawnWeight += ip.spawnChanceWeight;
+        }
     }
 
     public void CreateSpawn()
     {
-        throw new System.NotImplementedException();
+        foreach (var ip in itemDefinitions)
+        {
+            whichToSpawn += ip.spawnChanceWeight;
+
+            if (whichToSpawn >= chosen)
+            {
+                ItemSpawned = Instantiate(ip.itemSpawnObject, transform.position, Quaternion.identity);
+
+                ItemMaterial = ItemSpawned.GetComponent<Renderer>();
+
+                ItemMaterial.material = ip.itemMaterial;
+
+
+                ItemType = ItemSpawned.GetComponent<ItemPickUp>();
+                ItemType.itemDefinition = ip;
+
+                break; //once found the required item, break out of the loop, otherwise it may spawn two items at once
+            }
+        }
     }
 }
