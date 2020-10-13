ac9dada5c1425b6edc9051839a491560d6cbd09f
Wasim <mwasimse@gmail.com>
2020-10-13 20:39:42 +0500

    Combat System (Swords and Shovels) - Initial setup, and Base combat scripts and scriptable object(s) implemented

---
+++ CombatSystem_SwordsAndShovels/Assets/6. Combat/Scripts/Attack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack
+{
+    private readonly int _damage;
+    private readonly bool _critical;
+
+    public Attack(int damage, bool critical)
+    {
+        _damage = damage;
+        _critical = critical;
+    }
+
+
+
+    public int Damage => _damage;
+    public bool IsCritical => _critical;
+}
---
+++ CombatSystem_SwordsAndShovels/Assets/6. Combat/Scripts/ScriptableObjects/AttackDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Attack.asset", menuName = "Attack/Base Attack")]
+public class AttackDefinition : ScriptableObject
+{
+    public float CoolDown;
+    public float Range;
+    public float MinDamage;
+    public float MaxDamage;
+    public float CriticalMultiplier;
+    public float CriticalChance;
+
+
+    public Attack CreateAttack(CharacterStats wielderStats, CharacterStats defenderStats)
+    {
+        float coreDamage = wielderStats.GetDamage();
+        coreDamage += Random.Range(MinDamage, MaxDamage); //constitutes our dice roll
+
+        bool isCritical = Random.value < CriticalChance;
+        if (isCritical)
+            coreDamage *= CriticalMultiplier;
+
+        if (defenderStats != null)
+            coreDamage -= defenderStats.GetDamage();
+
+        return new Attack((int)coreDamage, isCritical);
+    }
+}
