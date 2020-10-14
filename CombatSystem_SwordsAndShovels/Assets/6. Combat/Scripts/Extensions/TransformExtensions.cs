718c4999d1813e493b49bc2f3ad1faf82b05528f
Wasim <mwasimse@gmail.com>
2020-10-14 12:31:55 +0500

    1) Created new WeaponDefinition (Scriptable object), and TransformExtensions to use in the WeaponDefinition. Created a new SwordAttack based on the WeaponDefinition and setup it's values in the Unity's inspector

---
+++ CombatSystem_SwordsAndShovels/Assets/6. Combat/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public static class TransformExtensions 
+{
+    private const float DOT_THREASHOLD = 0.5f;
+
+    public static bool IsFacingTarget(this Transform transform, Transform targetTransform)
+    {
+        /*
+         *  Dot Product returns values between 1 and -1
+         *  If value = 1, both are facing the same direction
+         *  -1, facing completely opposite direction
+         *  0, if two vectors are diagonal. If they form a right-angle (90 degrees) to each other.         
+         */
+        var vectorToTarget = targetTransform.position - transform.position;
+
+        //ensure the vector is normalized
+        vectorToTarget.Normalize();
+
+        //perform dot product operation
+        var dot = Vector3.Dot(transform.forward, vectorToTarget);
+
+        return dot > DOT_THREASHOLD;
+    }
+}
---
+++ CombatSystem_SwordsAndShovels/Assets/6. Combat/Scripts/ScriptableObjects/WeaponDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon.asset", menuName = "Attack/Weapon")]
+public class WeaponDefinition : AttackDefinition
+{
+    public Rigidbody weaponPrefab;
+
+    public void ExecuteAttack(GameObject attacker, GameObject defender)
+    {
+        if (defender == null) return;
+
+        //check defender is within the range of the attacker
+        if (Vector3.Distance(attacker.transform.position, defender.transform.position) > Range) return;
+
+        //Check if the defender is infront of the player
+        if (!attacker.transform.IsFacingTarget(defender.transform)) return;
+
+        //at this point the attack happens
+        var attackerStats = attacker.GetComponent<CharacterStats>();
+        var defenderStats = attacker.GetComponent<CharacterStats>();
+
+        var attack = CreateAttack(attackerStats, defenderStats);
+
+        //iterate over all of the behaviors implementing the IAttackable
+        var attackables = defender.GetComponentsInChildren<IAttackable>();
+
+        foreach (var attackable in attackables)
+        {
+            //call the OnAttack method
+            attackable.OnAttack(attacker, attack);
+        }
+    }
+}
