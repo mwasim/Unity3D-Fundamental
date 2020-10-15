d3a5c4cf0c26bec0a559ebf7e9c545fe77dc89fd
Wasim <mwasimse@gmail.com>
2020-10-15 17:09:41 +0500

    Script Optimization demo project - initial setup

---
+++ Script_Optimization/Assets/Scripts/Module 3/Demo_01/PlayerShipController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerShipController : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    
+    private Transform myTransform;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+
+        InvokeRepeating("Shoot", .33f, .33f);
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, myTransform.position, Quaternion.identity);
+    }
+}
