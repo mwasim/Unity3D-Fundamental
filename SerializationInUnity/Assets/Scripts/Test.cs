4ae11805fcbd565252d70d1c2c08cdd28a243765
Wasim <mwasimse@gmail.com>
2020-10-16 17:04:07 +0500

    Saving/Loading object's JSON string to/from PlayerPrefs

--- SerializationInUnity/Assets/Scripts/Test.cs
+++ SerializationInUnity/Assets/Scripts/Test.cs
@@ -33,14 +33,37 @@ public class Test : MonoBehaviour
     public TestClass cl;
     public TestStruct st;
 
+    private const string PlayerPrefsKey = "JsonTest";
+
+    private void OnEnable()
+    {
+        //load player prefs
+        JsonUtility.FromJsonOverwrite(PlayerPrefsKey, this);
+
+        Debug.Log("Player prefs are loaded.");
+    }
+
+    private void OnDisable()
+    {
+        //save player prefs
+
+        //We can save this object also to the PlayerPrefs
+        PlayerPrefs.SetString(PlayerPrefsKey, JsonUtility.ToJson(this, true));
+        PlayerPrefs.Save();
+
+        Debug.Log("Player prefs are saved.");
+    }
+
     //This method runs when a change is made to the serialized information in the editor
     private void OnValidate()
     {
         //PlayerPrefs - Store and access Player preferences between game sessions
         //On Windows this info is saved in the Registery, and on Mac, this info is saved in the Library preferences folder
         //On Mac, this info got saved on Library/Preferences/unity.GameDev.Game saves and serialization.plist (e.g. TestInt = 34)
-        PlayerPrefs.SetInt("TestInt", 34);
-        PlayerPrefs.Save();
+        //PlayerPrefs.SetInt("TestInt", 34);
+        //PlayerPrefs.Save();
+
+        
 
         Debug.Log("OnValidate method called..");
     }    
