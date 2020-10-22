ca73b051c01ce2b63d7fc1a6cb60071f976f7492
Wasim <mwasimse@gmail.com>
2020-10-22 15:00:28 +0500

    Added Event music snapshot and tested it

--- AudioFundamentals/Assets/AudioManager.cs
+++ AudioFundamentals/Assets/AudioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
@@ -9,13 +10,33 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource _mainAudioSource;
     [SerializeField] private AudioSource _auxAudioSource;
     [SerializeField] private AudioSource _ambientAudioSource;
+    [SerializeField] private AudioSource _eventAudioSource;
+
+    [SerializeField] private AudioMixerSnapshot _eventSnapshot;
+    [SerializeField] private AudioMixerSnapshot _idleSnapshot;
 
     public AudioSource MainAudioSource => _mainAudioSource;
     public AudioSource AuxAudioSource => _auxAudioSource;
     public AudioSource AmbienceAudioSource => _ambientAudioSource;
+    public AudioSource EventAudioSource => _eventAudioSource;
 
     private void Awake()
     {
         Instance = this;
+    }
+
+
+    public IEnumerator PlayEventMusic()
+    {
+        _eventSnapshot.TransitionTo(0.25f);
+        yield return new WaitForSeconds(0.3f);
+
+        _eventAudioSource.Play();
+
+        while (_eventAudioSource.isPlaying) yield return null;
+
+        _idleSnapshot.TransitionTo(025f);
+
+        yield break;
     }
 }
---
+++ AudioFundamentals/Assets/EventMusicCube.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMusicCube : MonoBehaviour
+{
+    [SerializeField] AudioClip _eventMusicClip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioManager.Instance.EventAudioSource.clip = _eventMusicClip;
+            AudioManager.Instance.StartCoroutine(nameof(AudioManager.Instance.PlayEventMusic));
+        }
+    }
+}
