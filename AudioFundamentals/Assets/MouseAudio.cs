05dea3fe0da0c01e78fb68f96e11970f76bff42e
Wasim <mwasimse@gmail.com>
2020-10-22 12:11:37 +0500

    Event Triggered SFX - Played OneShot clip on mouse click

---
+++ AudioFundamentals/Assets/MouseAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClip(AudioClip clip)
+    {
+        _audioSource.PlayOneShot(clip);
+    }
+}
