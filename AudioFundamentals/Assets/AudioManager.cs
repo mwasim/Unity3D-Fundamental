8cff51d274ab4022b9d697061f4680a5da252b47
Wasim <mwasimse@gmail.com>
2020-10-22 14:37:53 +0500

    Created Audio Manager singleton, and switch track zone to play different background music

---
+++ AudioFundamentals/Assets/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] private AudioSource _mainAudioSource;
+    [SerializeField] private AudioSource _auxAudioSource;
+    [SerializeField] private AudioSource _ambientAudioSource;
+
+    public AudioSource MainAudioSource => _mainAudioSource;
+    public AudioSource AuxAudioSource => _auxAudioSource;
+    public AudioSource AmbienceAudioSource => _ambientAudioSource;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
---
+++ AudioFundamentals/Assets/SwitchTrackCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTrackCube : MonoBehaviour
+{
+    [SerializeField] AudioClip _mainMusicClip;
+    [SerializeField] AudioClip _auxMusicClip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioManager.Instance.MainAudioSource.clip = _mainMusicClip;
+            AudioManager.Instance.AuxAudioSource.clip = _auxMusicClip;
+
+            AudioManager.Instance.MainAudioSource.Play();
+            AudioManager.Instance.AuxAudioSource.Play();
+        }
+    }
+}
