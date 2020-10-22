98e9362b9f2d5c0686124aafb365ced892080f9f
Wasim <mwasimse@gmail.com>
2020-10-22 12:07:04 +0500

    Played OneShot sound (water splash) on trigger enter and exit (as the player enters into the water and out of it)

---
+++ AudioFundamentals/Assets/PlayerAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] private AudioClip _splashSound;
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            _audioSource.PlayOneShot(_splashSound);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            _audioSource.PlayOneShot(_splashSound);
+        }
+    }
+}
